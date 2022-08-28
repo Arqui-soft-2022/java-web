0e46ef0966689d514bb024f55d066199713bb37d
Santiago Serrano <62350078+SantiagoAndresSerrano@users.noreply.github.com>
2022-08-28 13:05:52 -0500

    login validations added

--- src/main/java/web/qrcode/controller/AuthController.java
+++ src/main/java/web/qrcode/controller/AuthController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import org.springframework.web.client.RestTemplate;
@@ -23,16 +24,22 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public String login(@ModelAttribute("usuario") Usuario usuario){
+    public String login(@ModelAttribute("usuario") Usuario usuario, Model model){
         RestTemplate restTemplate = new RestTemplate();
         HttpEntity<Usuario> request = new HttpEntity<>(usuario);
-        ResponseEntity<String> response = restTemplate
-                .exchange(URI+"/auth/login", HttpMethod.POST, request, String.class);
-                
-        if(response.getStatusCode().value() == 200){
-            return "login";
-        }
+        try {
+            ResponseEntity<String> response = restTemplate
+            .exchange(URI+"/auth/login", HttpMethod.POST, request, String.class);
+            log.info(""+response.getBody());
+            if(response.getStatusCode().value() == 200){
+                return "login";
+            }
+        } catch (Exception e) {
+            model.addAttribute("mensaje","Usuario o contraseña incorrecta");
         return "login";
+
+        }
+        return "redirect:/auth/loginView";
     }
 
 
